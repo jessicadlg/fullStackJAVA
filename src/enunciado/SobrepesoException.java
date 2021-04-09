30c29476ec94910165338edfcd53b4315ba44705
jessicadlg <49699997+jessicadlg@users.noreply.github.com>
2021-04-09 16:12:14 -0300

    Delete src/enunciado directory

--- src/enunciado/SobrepesoException.java
+++
@@ -1,7 +0,0 @@
-package enunciado;
-
-public class SobrepesoException extends Exception {
-	public SobrepesoException() {
-		super("La carga tiene sobrepeso.");
-	}
-}
--- src/enunciado/VehiculoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package enunciado;
-
-public class VehiculoNotFoundException extends Exception {
-	public VehiculoNotFoundException() {
-		super ("No se encontró el vehiculo.");
-	}
-}
