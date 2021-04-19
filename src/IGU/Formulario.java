ed26c73ecc998bdb8c938f2c9a67e594e2664cd8
jessica <jessica_dlg@live.com.ar>
2021-04-19 12:49:04 -0300

    version 3 formulario agregando funcionalidad botones terminada

--- src/IGU/Formulario.java
+++ src/IGU/Formulario.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package IGU;
 
 public class Formulario extends javax.swing.JFrame {
 
     //declaro los vectores
-    String dnis [] = new String[10];
-    String nombres [] = new String[10];
-    String apellidos [] = new String[10];
-    String direcciones [] = new String[10];
-    String telefonos [] = new String[10];
-    String nacimientos [] = new String[10];
+    String dnis[] = new String[10];
+    String nombres[] = new String[10];
+    String apellidos[] = new String[10];
+    String direcciones[] = new String[10];
+    String telefonos[] = new String[10];
+    String nacimientos[] = new String[10];
     int posicion = 0;
 
     public Formulario() {
@@ -33,7 +28,6 @@ public class Formulario extends javax.swing.JFrame {
         btnAvanzar = new javax.swing.JButton();
         btnRetroceder = new javax.swing.JButton();
         IndiceLABEL = new javax.swing.JLabel();
-        campoPosicion = new javax.swing.JTextField();
         dniText = new javax.swing.JTextField();
         direccionText = new javax.swing.JTextField();
         btnGuardar = new javax.swing.JButton();
@@ -45,6 +39,7 @@ public class Formulario extends javax.swing.JFrame {
         nacimientoText = new javax.swing.JTextField();
         apellidoText = new javax.swing.JTextField();
         apellidoLABEL = new javax.swing.JLabel();
+        campoPosicion = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -55,7 +50,7 @@ public class Formulario extends javax.swing.JFrame {
 
         dniLABEL.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         dniLABEL.setForeground(new java.awt.Color(0, 0, 102));
-        dniLABEL.setText("DNI         :");
+        dniLABEL.setText("DNI               :");
 
         direccionLABEL.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         direccionLABEL.setForeground(new java.awt.Color(0, 0, 102));
@@ -78,8 +73,6 @@ public class Formulario extends javax.swing.JFrame {
         IndiceLABEL.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         IndiceLABEL.setForeground(new java.awt.Color(0, 0, 102));
         IndiceLABEL.setText("Indice:");
-
-        campoPosicion.setEditable(false);
 
         btnGuardar.setText("Guardar");
         btnGuardar.addActionListener(new java.awt.event.ActionListener() {
@@ -104,6 +97,9 @@ public class Formulario extends javax.swing.JFrame {
         apellidoLABEL.setForeground(new java.awt.Color(0, 0, 102));
         apellidoLABEL.setText("Apellidos       :");
 
+        campoPosicion.setEditable(false);
+        campoPosicion.setText("0");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -118,14 +114,14 @@ public class Formulario extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createSequentialGroup()
                                     .addGap(77, 77, 77)
                                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                                 .addComponent(dniLABEL)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                                 .addComponent(dniText, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE))
                                             .addGroup(jPanel1Layout.createSequentialGroup()
                                                 .addComponent(nombreLABEL)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                                 .addComponent(nombreText, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                         .addGroup(jPanel1Layout.createSequentialGroup()
                                             .addComponent(apellidoLABEL)
@@ -153,8 +149,8 @@ public class Formulario extends javax.swing.JFrame {
                                             .addGap(8, 8, 8)))
                                     .addGap(27, 27, 27)
                                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(campoPosicion)
-                                        .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE))
+                                        .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)
+                                        .addComponent(campoPosicion))
                                     .addGap(38, 38, 38)
                                     .addComponent(btnAvanzar, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE))))
                         .addGroup(jPanel1Layout.createSequentialGroup()
@@ -196,9 +192,9 @@ public class Formulario extends javax.swing.JFrame {
                     .addComponent(btnAvanzar))
                 .addGap(26, 26, 26)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(campoPosicion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(IndiceLABEL))
-                .addContainerGap(14, Short.MAX_VALUE))
+                    .addComponent(IndiceLABEL)
+                    .addComponent(campoPosicion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -219,41 +215,59 @@ public class Formulario extends javax.swing.JFrame {
 
 
     private void btnRetrocederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRetrocederActionPerformed
-        //guardo indice
+        //guardo indice de String a int
         posicion = Integer.parseInt(campoPosicion.getText());
+
         //limito indice:
-        if(posicion>0){
-            posicion-= -1;
+        if (posicion >0) {
+            posicion = posicion - 1;
         }
-        //modifico indice
+        //modifico indice de int a String para mostrar
         campoPosicion.setText(Integer.toString(posicion));
+
         //actualizo todos los campos de esa posicion
         dniText.setText(dnis[posicion]);
+        nombreText.setText(nombres[posicion]);
+        apellidoText.setText(apellidos[posicion]);
+        direccionText.setText(direcciones[posicion]);
+        telefonoText.setText(telefonos[posicion]);
+        nacimientoText.setText(nacimientos[posicion]);
         
+       
     }//GEN-LAST:event_btnRetrocederActionPerformed
-    // String dni;
+   
     private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
         //casteo 
         posicion = Integer.parseInt(campoPosicion.getText());
+
         //guardo el ingreso
         dnis[posicion] = dniText.getText();
+        nombres[posicion] = nombreText.getText();
+        apellidos[posicion] = apellidoText.getText();
+        direcciones[posicion] = direccionText.getText();
+        telefonos[posicion] = telefonoText.getText();
+        nacimientos[posicion] = nacimientoText.getText();
         
-       
-        
+         
     }//GEN-LAST:event_btnGuardarActionPerformed
 
     private void btnAvanzarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAvanzarActionPerformed
-       //guardo indice
+        //guardo indice
         posicion = Integer.parseInt(campoPosicion.getText());
         //limito indice:
-        if(posicion<10){
-            posicion-= +1;
+        if (posicion <9) {
+            posicion = posicion + 1;
         }
         //modifico indice
         campoPosicion.setText(Integer.toString(posicion));
         //actualizo todos los campos de esa posicion
         dniText.setText(dnis[posicion]);
-        
+        nombreText.setText(nombres[posicion]);
+        apellidoText.setText(apellidos[posicion]);
+        direccionText.setText(direcciones[posicion]);
+        telefonoText.setText(telefonos[posicion]);
+        nacimientoText.setText(nacimientos[posicion]);
+
     }//GEN-LAST:event_btnAvanzarActionPerformed
 
 
